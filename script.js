3ed696fb3ca51b432dc3d92a1786981574dd771b
Achra-f <achraf.bendella@student.odisee.be>
2022-07-18 17:47:45 +0200

    adding buttons to RPS game

--- script.js
+++ script.js
@@ -1,3 +1,99 @@
+// Rock paper scissors javascript TOP
+
+const options = document.querySelectorAll('.options');
+
+
+options.forEach((option) => {
+    option.addEventListener("click", function () {
+        const pInput = this.textContent;
+        const cOptions = ["Rock", "Paper", "Scissors"];
+        const cInput = cOptions[Math.floor(Math.random() * 3)];
+
+        decideWinner(pInput, cInput);
+
+    });
+});
+
+
+
+function decideWinner(pInput, cInput) {
+    const currentMatch = `${pInput} vs ${cInput}`;
+
+    // Tie check
+    if (pInput === cInput) {
+      alert(`${currentMatch} is a Tie`);
+      return;
+    }
+  
+    // Rock
+    if (pInput === "Rock") {
+      if (cInput === "Scissors") {
+        alert(`${currentMatch} = You Win`);
+      } else {
+        alert(`${currentMatch} = Computer Wins`);
+      }
+    }
+    // Paper
+    else if (pInput === "Paper") {
+      if (cInput === "Rock") {
+        alert(`${currentMatch} = You Win`);
+      } else {
+        alert(`${currentMatch} = Computer Wins`);
+      }
+    }
+    // Scissors
+    else {
+      if (cInput === "Paper") {
+        alert(`${currentMatch} = You Win`);
+      } else {
+        alert(`${currentMatch} = Computer Wins`);
+      }
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Rock paper scissors without GUI
 console.log("Welcome To Rock Paper Scissors Game!!!");
 
 
@@ -66,6 +162,7 @@ function game() {
 }
 */
 
+/*
 function game(){
 
     for (let i = 0; i < 1000; i++) {
@@ -97,5 +194,7 @@ function game(){
     }
 }
 
+
 game();
 
+*/
